a69a945f18248c09440408d652b49c833dc91c35
Athea DeLing <almdeling@gmail.com>
2022-10-08 13:35:08 -0700

    add inital form js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import GalaticAge from './galaticAge';
 
-function handleTriangleForm() {
+function handleGalaticForm() {
   event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+  const age = document.querySelector('#number').value;
+  const galaticAge = new GalaticAge(age);
+  document.getElementById('mercury').innerText;
+  document.getElementById('venus').innerText;
+  document.getElementById('mars').innerText;
+  document.getElementById('jupiter').innerText;
 }
 
-window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
-});
+
+  document.querySelector("#galatic-form").addEventListener("submit", handleGalaticForm);
